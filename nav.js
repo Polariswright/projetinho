ae76969bf85e8525b3370f5783d1c2c776510682
Polaris <108342258+Polariswright@users.noreply.github.com>
2022-09-16 08:31:13 -0300

    Update nav.js

--- nav.js
+++ nav.js
@@ -4,5 +4,5 @@ fetch('nav.html') //o fetch
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
+    oldelem.parentNode.replaceChild(newelem,oldelem)
 })
